97537c3a5c922299bb80dc80b83ac600d8d9e9ce
kaixiang.qiu <kaixiang.qiu@guwave.com>
2025-05-06 10:18:32 +0800

    test: deepseek model切换

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,4 +1,5 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
+import { createDeepSeek } from '@ai-sdk/deepseek';
 
 export function getAnthropicModel(apiKey: string) {
   const anthropic = createAnthropic({
@@ -6,4 +7,13 @@ export function getAnthropicModel(apiKey: string) {
   });
 
   return anthropic('claude-3-5-sonnet-20240620');
+}
+const Deepseek_API_KEY = 'sk-2c735492c36f49268b963bea841929f6';
+
+export function getDeepSeekModel() {
+  const deepseek = createDeepSeek({
+    apiKey: Deepseek_API_KEY,
+  });
+
+  return deepseek('deepseek-chat');
 }

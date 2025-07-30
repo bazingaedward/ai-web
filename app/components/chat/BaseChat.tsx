50f12cbfff8233603d6ab89cb9e0783970879657
Edward Qiu <bazingaedward@gmail.com>
2025-07-30 21:01:26 +0800

    feat: check login status

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -131,7 +131,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                       minHeight: TEXTAREA_MIN_HEIGHT,
                       maxHeight: TEXTAREA_MAX_HEIGHT,
                     }}
-                    placeholder="How can Bolt help you today?"
+                    placeholder="How can Sharkbook help you today?"
                     translate="no"
                   />
                   <ClientOnly>
